32d913799fb7deafffc85e45b0f81dc8ecd5db43
axel <iazzourene01@gmail.com>
2026-08-01 15:53:32 +0100

    changes

---
+++ assets/js/products.js
@@ -0,0 +1,120 @@
+const products = [
+
+{
+id:"tshirt-classique",
+nom:"T-Shirt Classique",
+categorie:"tshirts",
+prix:"À partir de 1200 DA",
+image:"assets/img/tshirt.jpg",
+description:"T-Shirt 100% coton disponible vierge ou personnalisé."
+},
+
+{
+id:"tshirt-oversize",
+nom:"T-Shirt Oversize",
+categorie:"tshirts",
+prix:"À partir de 1600 DA",
+image:"assets/img/oversize.jpg",
+description:"Coupe oversize moderne, idéale pour les marques et événements."
+},
+
+{
+id:"polo-simple",
+nom:"Polo Classique",
+categorie:"polos",
+prix:"À partir de 1800 DA",
+image:"assets/img/polo.jpg",
+description:"Polo professionnel personnalisable."
+},
+
+{
+id:"polo-long",
+nom:"Polo Manches Longues",
+categorie:"polos",
+prix:"À partir de 2100 DA",
+image:"assets/img/polo-long.jpg",
+description:"Polo manches longues pour entreprises."
+},
+
+{
+id:"tricot-simple",
+nom:"Tricot Simple",
+categorie:"tricots",
+prix:"À partir de 2400 DA",
+image:"assets/img/tricot.jpg",
+description:"Tricot professionnel confortable."
+},
+
+{
+id:"tricot-polo",
+nom:"Tricot Polo",
+categorie:"tricots",
+prix:"À partir de 2600 DA",
+image:"assets/img/tricot-polo.jpg",
+description:"Tricot style polo."
+},
+
+{
+id:"hoodie",
+nom:"Hoodie",
+categorie:"hoodies",
+prix:"À partir de 2900 DA",
+image:"assets/img/hoodie.jpg",
+description:"Hoodie personnalisable."
+},
+
+{
+id:"tablier",
+nom:"Tablier de Cuisine",
+categorie:"cuisine",
+prix:"À partir de 900 DA",
+image:"assets/img/tablier.jpg",
+description:"Tablier professionnel."
+},
+
+{
+id:"chemise-cuisine",
+nom:"Chemise de Cuisine",
+categorie:"cuisine",
+prix:"À partir de 2500 DA",
+image:"assets/img/chemise-cuisine.jpg",
+description:"Chemise de cuisine professionnelle."
+},
+
+{
+id:"gilet",
+nom:"Gilet de Travail",
+categorie:"travail",
+prix:"À partir de 1700 DA",
+image:"assets/img/gilet.jpg",
+description:"Gilet de sécurité personnalisable."
+},
+
+{
+id:"dossard-ouvert",
+nom:"Dossard Ouvert",
+categorie:"travail",
+prix:"À partir de 850 DA",
+image:"assets/img/dossard-ouvert.jpg",
+description:"Dossard ouvert professionnel."
+},
+
+{
+id:"dossard-ferme",
+nom:"Dossard Fermé",
+categorie:"travail",
+prix:"À partir de 950 DA",
+image:"assets/img/dossard-ferme.jpg",
+description:"Dossard fermé personnalisable."
+},
+
+{
+id:"sport",
+nom:"Dossard Sport",
+categorie:"sport",
+prix:"À partir de 700 DA",
+image:"assets/img/sport.jpg",
+description:"Dossard pour clubs sportifs."
+}
+
+];
